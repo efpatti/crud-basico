853b0fcc7b226c64a13a686a46ff3df6f2018ae2
Enzo Patti <enzo.patti@aluno.senai.br>
2025-03-27 19:30:33 -0300

    sweet alert adicionado

--- js/script.js
+++ js/script.js
@@ -11,15 +11,15 @@ document.addEventListener("DOMContentLoaded", () => {
    userList.innerHTML = users
     .map(
      (user) => `
-                     <tr class="border-b hover:bg-gray-50">
-                         <td class="py-4 px-6">${user.name}</td>
-                         <td class="py-4 px-6">${user.type}</td>
-                         <td class="py-4 px-6 text-center space-x-2">
-                         <button onclick="editUser(${user.id}, '${user.name}', '${user.type}')" class="text-red-500 hover:text-yellow-700">Editar</button>
-                             <button onclick="deleteUser(${user.id})" class="text-red-500 hover:text-red-700">Excluir</button>
-                         </td>
-                     </tr>
-                 `
+                        <tr class="border-b hover:bg-gray-50">
+                            <td class="py-4 px-6">${user.name}</td>
+                            <td class="py-4 px-6">${user.type}</td>
+                            <td class="py-4 px-6 text-center space-x-2">
+                            <button onclick="editUser(${user.id}, '${user.name}', '${user.type}')" class="bg-yellow-500/80 hover:bg-yellow-500/70 text-slate-200 p-2 rounded-lg">Editar</button>
+                                <button onclick="deleteUser(${user.id})" class="bg-red-500/80 hover:bg-red-500/70 text-slate-200 p-2 rounded-lg">Excluir</button>
+                            </td>
+                        </tr>
+                    `
     )
     .join("");
   } catch (error) {
@@ -42,7 +42,8 @@ document.addEventListener("DOMContentLoaded", () => {
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ name, type }),
    });
-   alert(await res.text());
+   const message = await res.text();
+   Swal.fire({ icon: "success", title: "Sucesso!", text: message });
    userForm.reset();
    userIdField.value = "";
    fetchUsers();
@@ -58,12 +59,25 @@ document.addEventListener("DOMContentLoaded", () => {
  };
 
  window.deleteUser = async (id) => {
-  if (!confirm("Tem certeza que deseja excluir?")) return;
+  const confirmDelete = await Swal.fire({
+   title: "Tem certeza?",
+   text: "Essa ação não pode ser desfeita!",
+   icon: "warning",
+   showCancelButton: true,
+   confirmButtonColor: "#d33",
+   cancelButtonColor: "#3085d6",
+   confirmButtonText: "Sim, excluir!",
+   cancelButtonText: "Cancelar",
+  });
+
+  if (!confirmDelete.isConfirmed) return;
+
   try {
    const res = await fetch(`http://localhost:${port}/${id}`, {
     method: "DELETE",
    });
-   alert(await res.text());
+   const message = await res.text();
+   Swal.fire({ icon: "success", title: "Excluído!", text: message });
    fetchUsers();
   } catch (error) {
    console.error("Erro ao excluir usuário", error);
